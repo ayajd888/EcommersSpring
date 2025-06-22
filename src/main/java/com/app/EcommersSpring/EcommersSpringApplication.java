01d8e28dfe53829422c2cc580facf3c769077f63
AyaZ Deshmukh <103880151+ayajd888@users.noreply.github.com>
2025-06-22 11:54:19 +0530

    Second Commit

--- src/main/java/com/app/EcommersSpring/EcommersSpringApplication.java
+++ src/main/java/com/app/EcommersSpring/EcommersSpringApplication.java
@@ -1,5 +1,9 @@
 package com.app.EcommersSpring;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +11,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EcommersSpringApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv= Dotenv.configure().load();
+
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
 		SpringApplication.run(EcommersSpringApplication.class, args);
 	}
 
